2e522c1d1df246452617f0c61533f8375eee25e1
Sohayla Ihab <77901496+PerfectionistAF@users.noreply.github.com>
2023-05-02 23:04:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,41 +4,6 @@ from PIL import Image
 import statistics
 import cv2
 
-def SpatialImprovement(imagePath, invBinary):
-    img = cv2.imread(imagePath)
-    reducenoise = cv2.GaussianBlur(img, (5, 5), 0)
-    sharpen_filter=np.array([[-1,-1,-1],
-                 [-1,9,-1],
-                [-1,-1,-1]])
-    sharp_image=cv2.filter2D(reducenoise,-1,sharpen_filter)
-    gray = cv2.cvtColor(sharp_image, cv2.COLOR_BGR2GRAY)
-    
-    ret,baseline = cv2.threshold(gray,130,255,cv2.THRESH_TRUNC)
-    if(invBinary):
-        ret,background = cv2.threshold(baseline,125,255,cv2.THRESH_BINARY_INV)
-        ret,foreground = cv2.threshold(baseline,125,255,cv2.THRESH_BINARY)
-    
-    else:
-        ret,background = cv2.threshold(baseline,125,255,cv2.THRESH_BINARY)
-        ret,foreground = cv2.threshold(baseline,125,255,cv2.THRESH_BINARY_INV) 
- 
-    foreground = cv2.bitwise_and(img,img, mask=foreground)  # Update foreground with bitwise_and to extract real foreground
- 
-    # Convert black and white back into 3 channel greyscale
-    background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)
- 
-    # Combine the background and foreground to obtain our final image
-    finalimage = background+foreground
-    sobelx = cv2.Sobel(finalimage,cv2.CV_64F,1,0)#,ksize=5)     
-    sobely = cv2.Sobel(finalimage,cv2.CV_64F,0,1)#,ksize=5)
-    laplacianapproximation = cv2.Laplacian(finalimage,cv2.CV_64F)
-    display1 = cv2.convertScaleAbs(finalimage)
-    display2 = cv2.normalize(display1, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8UC1)
-    cv2.imshow('Sobel Transform', display2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
 def LocalizeDigits(imagePath, lowerAreaThreshold, upperAreaThreshold, thresholdType, invBinary):
     img = cv2.imread(imagePath)
 
@@ -93,9 +58,5 @@ def LocalizeDigits(imagePath, lowerAreaThreshold, upperAreaThreshold, thresholdT
 for filename in os.listdir("PROJECT FILES/train/train"):
 #for filename in range(10):
     f = os.path.join("PROJECT FILES/train/train", filename)
-    SpatialImprovement(f, False)
-    SpatialImprovement(f, True)
-    #bgremove2(f)
-    #medianFilter(SpatialImprovement(f))
-    #LocalizeDigits(f, 30, 5000, "adaptiveMean", False)
-    #LocalizeDigits(f, 30, 5000, "adaptiveMean", True)'''
+    LocalizeDigits(f, 30, 5000, "adaptiveMean", False)
+    LocalizeDigits(f, 30, 5000, "adaptiveMean", True)'''
